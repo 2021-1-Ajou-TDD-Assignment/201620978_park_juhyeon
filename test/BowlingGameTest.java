42a4d55fbeb29c7ef2d1795cd3f55172e3af5af3
ParkJoohyun <terajoohyun@ajou.ac.kr>
2021-04-06 02:06:54 +0900

    The second test

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -1,13 +1,28 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest {
+	private Game g;
+	
+	@Before
+	public void setUp() {
+		g = new Game();
+	}
+	
 	@Test
 	public void testGutterGame() {
-		Game g = new Game();
 		for (int i=0; i<20; i++)
 			g.roll(0);
 		assertEquals(0, g.score());
+	}
+
+	
+	@Test
+	public void testAllOnes() {
+		for(int i=0 ; i<20 ; i++) 
+			g.roll(1);
+		assertEquals(20, g.score());
 	}
 }
