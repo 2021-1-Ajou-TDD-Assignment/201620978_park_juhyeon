d4d2e5d2ed6a607c9c5fb7d75cdb37acb44cfcc3
TERAJOO <55892515+terajh@users.noreply.github.com>
2021-04-05 21:11:48 +0900

    The first test

---
+++ test/BowlingGameTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BowlingGameTest {
+	@Test
+	public void testGutterGame() {
+		Game g = new Game();
+		for (int i=0; i<20; i++)
+			g.roll(0);
+		assertEquals(0, g.score());
+	}
+}
