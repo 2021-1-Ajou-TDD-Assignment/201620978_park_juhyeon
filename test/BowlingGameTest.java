dc75c79927d91884cecc4fac6ef9e916805904ea
ParkJoohyun <terajoohyun@ajou.ac.kr>
2021-04-06 02:22:56 +0900

    The fourth test

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -36,6 +36,19 @@ public class BowlingGameTest {
 		rollMany(17, 0);
 		assertEquals(16,g.score());
 	}
+	
+	@Test 
+	public void	testOneStrike () {
+		rollStrike();
+		g.roll(3);
+		g.roll(4);
+		rollMany(16, 0);
+		assertEquals(24,g.score());
+	}
+
+	private void rollStrike() {
+		g.roll(10); // strike
+	}
 
 	private void rollSpare() {
 		g.roll(5);
